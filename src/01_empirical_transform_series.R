710ab838b449e19bd7fd0fec360c366b5ec6b12a
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-14 18:35:57 +0200

    fix data path

--- src/01_empirical_transform_series.R
+++ src/01_empirical_transform_series.R
@@ -2,7 +2,20 @@ library(readxl)
 library(ggplot2)
 library(data.table)
 
-dt <- read_xlsx("data/Nelson_Plosser_data.xlsx") |>
+dt <- read_xlsx("data/raw/Nelson_Plosser_data.xlsx") |>
   setDT()
 
 series <- names(dt)[-1]
+
+dt_plot <- dt |>
+  melt(
+    id.vars = "Year",
+    measure.vars = series
+  )
+
+dt_plot |>
+  ggplot(aes(x = Year, y = value, group = variable, color = variable)) +
+  geom_line(show.legend = FALSE) +
+  facet_wrap(~variable, scales = "free_y") +
+  labs(x = NULL, y = NULL) +
+  theme_light()

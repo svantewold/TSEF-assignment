1191faec70c89a77a6b1eeb2483a7e0e00cd10b8
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-15 16:09:13 +0200

    add plot for log cpi series

--- src/02_empirical_transform_series.R
+++ src/02_empirical_transform_series.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(ggplot2)
 
 dt <- fread(
   "data/processed/nelson_plosser_data.csv"
@@ -13,3 +14,22 @@ fwrite(
   dt,
   "data/processed/cpi_series.csv"
 )
+
+dt |>
+  ggplot(aes(x = year, y = log_cpi)) +
+  geom_line(color = "steelblue", lwd = 1) +
+  labs(x = NULL, y = NULL) +
+  scale_y_continuous(n.breaks = 7) +
+  scale_x_continuous(n.breaks = 10) +
+  coord_cartesian(
+    ylim = c(3.15, 4.85),
+    xlim = c(1857, 1973),
+    expand = FALSE
+  ) +
+  theme_light() +
+  theme(
+    axis.text = element_text(color = "black"),
+    panel.grid.minor = element_blank(),
+    panel.grid = element_line(color = "grey85")
+  )
+ggsave("output/log_cpi_series.pdf", dpi = 300, width = 6, height = 4)

a8d25502f3c4011e15fcca462b63439c0ca829a9
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-16 20:15:59 +0200

    fix math latex character for table

--- src/03_empirical_unit_root_testing.R
+++ src/03_empirical_unit_root_testing.R
@@ -27,7 +27,7 @@ test_output <- data.frame(
     "f.value" = test$F[2],
     "critical.val.95" = 6.49,
     "critical.val.99" = 8.73,
-    "dickey.f.teststat" = "$\phi_3$"
+    "dickey.f.teststat" = "$\\phi_3$"
 )
 
 latex_table <- kable(
@@ -40,7 +40,8 @@ latex_table <- kable(
         "Critical value for 95% confidence interval",
         "Critical value for 99% confidence interval",
         "Dickey-Fuller Test Statistic"
-    )
+    ),
+    escape = FALSE
 )
 
 write(latex_table, "output/tables/unit_root_test.tex")

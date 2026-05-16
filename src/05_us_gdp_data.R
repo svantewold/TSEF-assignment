822df55c7da3045cdffde15b19e88df757a99e26
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-16 23:46:30 +0200

    gdp data preparation for analysis

---
+++ src/05_us_gdp_data.R
@@ -0,0 +1,20 @@
+library(readxl)
+library(data.table)
+
+dt <- read_xlsx(
+  "data/raw/real_gdp_US_2022Q4.xlsx"
+) |>
+  setDT()
+
+dt[,
+  ":="(
+    date = strptime(dt$dates, format = "%d-%b-%Y") |> as.IDate(),
+    log_gdp = log(gdpc1)
+  )
+] |>
+  _[dates < "2020-01-01", .(date, log_gdp, nber = NBER)]
+
+fwrite(
+  dt,
+  "data/processed/us_gdp_series.csv"
+)

9b6e76955caf1f3d24ce35f30e82019555dfcf09
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-17 19:34:44 +0200

    edit repo root runner

--- src/00_run_all.R
+++ src/00_run_all.R
@@ -3,7 +3,9 @@ source("src/02_empirical_transform_series.R")
 source("src/03_empirical_unit_root_testing.R")
 
 if (!file.exists("data/simulated/simulated_series_estimation.csv")) {
-    source("src/04_simulated_random_walk_estimation.R")
+    suppressWarnings(
+        source("src/04_simulated_random_walk_estimation.R")
+    )
 } else {
     message("Simulated data is already present, skipping simulation.")
 }
@@ -12,5 +14,9 @@ source("src/05_simulated_random_walk_analysis.R")
 source("src/06_us_gdp_data.R")
 source("src/07_us_gdp_decompositions.R")
 
-gert::git_commit_all("Repo root runner: commit output")
+suppressMessages(
+    quiet(gert::git_commit_all("Repo root runner: commit output"))
+)
+message("Commited output files via git.")
+
 message("Repository root ran successfully!")

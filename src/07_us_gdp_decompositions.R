2a0389116fa637d9f00f7cd8a3bc7aba06d0c707
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-18 00:06:37 +0200

    implement beveridge nelson decomposition and hp filter, and plot transitory component

--- src/07_us_gdp_decompositions.R
+++ src/07_us_gdp_decompositions.R
@@ -1,12 +1,30 @@
-library(data.table)
-library(Matrix)
-import::from("src/lib/functions.R", hp_filter)
+library(data.table, warn.conflicts = FALSE)
+library(Matrix, warn.conflicts = FALSE)
+library(ggplot2, warn.conflicts = FALSE)
+import::from("src/lib/functions.R", hp_filter, bn_decomposition)
 
 dt <- fread("data/processed/us_gdp_series.csv")
 
 dt[,
   ":="(
-    hp_transitory = hp_filter(log_gdp, 1600)$cycle,
-    hp_permanent = hp_filter(log_gdp, 1600)$trend
+    bn_permanent = bn_decomposition(log_gdp)$trend,
+    bn_transitory = bn_decomposition(log_gdp)$cycle,
+    hp_permanent = hp_filter(log_gdp, 1600)$trend,
+    hp_transitory = hp_filter(log_gdp, 1600)$cycle
   )
 ]
+
+melt(
+  dt,
+  id.vars = "date",
+  measure.vars = c("bn_transitory", "hp_transitory")
+) |>
+  ggplot(aes(x = date, color = variable)) +
+  geom_line(aes(y = value), lwd = 1) +
+  labs(x = NULL, y = NULL) +
+  theme_light() +
+  theme(
+    axis.text = element_text(color = "black"),
+    panel.grid.minor = element_blank(),
+    panel.grid = element_line(color = "grey85")
+  )

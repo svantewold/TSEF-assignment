2fa6cf26cccf2a8d31fe7b32eac6ce9abe8394f8
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-17 19:43:52 +0200

    further edits to project entry point

--- src/00_run_all.R
+++ src/00_run_all.R
@@ -7,18 +7,18 @@ if (!file.exists("data/simulated/simulated_series_estimation.csv")) {
         source("src/04_simulated_random_walk_estimation.R")
     )
 } else {
-    message("Simulated data is already present, skipping simulation.")
+    message("- Simulated data is already present, skipping simulation.")
 }
 
 source("src/05_simulated_random_walk_analysis.R")
 source("src/06_us_gdp_data.R")
 source("src/07_us_gdp_decompositions.R")
 
-suppressMessages(
+invisible(
     gert::git_commit_all(
         "Project entry point: ran and commited output files."
     )
 )
-message("Commited output files via git.")
+message("- Commited output files via git.\n\n")
 
 message("Repository root ran successfully!")

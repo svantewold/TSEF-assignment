ecdf42d2fe1d61cf3f7a82e4a854abcfb1b4894c
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-17 19:35:29 +0200

    edit repo root runner

--- src/00_run_all.R
+++ src/00_run_all.R
@@ -15,7 +15,9 @@ source("src/06_us_gdp_data.R")
 source("src/07_us_gdp_decompositions.R")
 
 suppressMessages(
-    quiet(gert::git_commit_all("Repo root runner: commit output"))
+    quiet(gert::git_commit_all(
+        "Project entry point: ran and commited output files."
+    ))
 )
 message("Commited output files via git.")
 

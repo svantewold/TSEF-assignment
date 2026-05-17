137afb1900cdbfa312d687967e4168cf6d1e43e6
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-18 00:26:39 +0200

    fix path for output

--- src/02_empirical_transform_series.R
+++ src/02_empirical_transform_series.R
@@ -32,4 +32,4 @@ dt |>
     panel.grid.minor = element_blank(),
     panel.grid = element_line(color = "grey85")
   )
-ggsave("output/log_cpi_series.pdf", dpi = 300, width = 5.5, height = 3)
+ggsave("output/figures/log_cpi_series.pdf", dpi = 300, width = 5.5, height = 3)

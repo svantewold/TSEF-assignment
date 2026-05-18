b7f9786f66ef1a1687c3dae0f9427d701b84c841
Svante Daneklev Wold <svantedw@icloud.com>
2026-05-18 16:53:47 +0200

    move warning suppression from script to project entry point

--- src/05_simulated_random_walk_analysis.R
+++ src/05_simulated_random_walk_analysis.R
@@ -63,7 +63,7 @@ histogram_t_stats <- dt |>
     ),
   )
 
-suppressWarnings(histogram_t_stats + histogram_estimates)
+histogram_t_stats + histogram_estimates
 ggsave(
   "output/figures/simulated_estimation_densities.pdf",
   width = 8,
